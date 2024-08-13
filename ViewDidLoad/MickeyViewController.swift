0509545ef875ab52bbc41912de8367f9093fbc6a
Adam <bgisgone@gmail.com>
2024-08-13 14:25:00 +0800

    自訂 controller 類別和利用 viewDidLoad 客製 App 畫面

---
+++ ViewDidLoad/MickeyViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MickeyViewController.swift
+//  ViewDidLoad
+//
+//  Created by Adam Chen on 2024/8/13.
+//
+
+import UIKit
+
+class MickeyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
