8ed230a66fa9679d961d7cb7dc1555564a143d23
Adam <bgisgone@gmail.com>
2024-08-13 14:25:29 +0800

    自訂 controller 類別和利用 viewDidLoad 客製 App 畫面 OK

--- ViewDidLoad/MickeyViewController.swift
+++ ViewDidLoad/MickeyViewController.swift
@@ -6,13 +6,103 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MickeyViewController: UIViewController {
 
+    @IBOutlet weak var fightButton: UIButton!
+    var looper: AVPlayerLooper?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        fightButton.isHidden = true
+        //設定Button字體＆大小＆顏色
+        var config = UIButton.Configuration.plain()
+        var title = AttributedString("FIGHT")
+        title.font = UIFont(name: "MonsterBlood", size: 35)
+        title.foregroundColor = UIColor(red: 180/255, green: 0, blue: 0, alpha: 1)
+        config.attributedTitle = title
+        fightButton.configuration = config
+        
+        //音樂播放
+        let url = Bundle.main.url(forResource: "bgm01", withExtension: "mp3")!
+        let player = AVQueuePlayer()
+        let item = AVPlayerItem(url: url)
+        looper = AVPlayerLooper(player: player, templateItem: item)
+        player.play()
+        
+        //漸層背景
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [
+            UIColor.black.cgColor,
+            CGColor(red: 77/255, green: 93/255, blue: 102/255, alpha: 1),
+            CGColor(red: 70/255, green: 0/255, blue: 0/255, alpha: 1)
+        ]
+        gradientLayer.locations = [0, 0.8, 1]
+        view.layer.insertSublayer(gradientLayer, at: 0)
+        
+        //畫Mickey
+        let mickey = CGRect(x: (view.frame.width - 300)/2, y: 100, width: 300, height: 300)
+        let mickeyView = UIView(frame: mickey)
+        mickeyView.backgroundColor = UIColor.clear
+        view.addSubview(mickeyView)
+        
+        let mickeyPath = UIBezierPath()
+        mickeyPath.move(to: CGPoint(x: 118, y: 96))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 109, y: 46), controlPoint: CGPoint(x: 127, y: 68))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 30, y: 46), controlPoint: CGPoint(x: 73, y: 13))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 16, y: 127), controlPoint: CGPoint(x: -12, y: 90))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 67, y: 142), controlPoint: CGPoint(x: 30, y: 144))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 70, y: 224), controlPoint: CGPoint(x: 49, y: 189))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 150, y: 270), controlPoint: CGPoint(x: 95, y: 270))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 230, y: 224), controlPoint: CGPoint(x: 205, y: 270))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 233, y: 142), controlPoint: CGPoint(x: 252, y: 189))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 283, y: 127), controlPoint: CGPoint(x: 270, y: 144))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 271, y: 47), controlPoint: CGPoint(x: 312, y: 90))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 192, y: 45), controlPoint: CGPoint(x: 227, y: 13))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 182, y: 96), controlPoint: CGPoint(x: 175, y: 68))
+        mickeyPath.addQuadCurve(to: CGPoint(x: 118, y: 96), controlPoint: CGPoint(x: 150, y: 80))
+        mickeyPath.close()
+        
+        let mickeyLayer = CAShapeLayer()
+        mickeyLayer.path = mickeyPath.cgPath
+        mickeyLayer.lineWidth = 5
+        mickeyLayer.strokeColor = UIColor(red: 180/255, green: 0, blue: 0, alpha: 1).cgColor
+        mickeyLayer.fillColor = UIColor.clear.cgColor
+        mickeyView.layer.addSublayer(mickeyLayer)
+        
+        //設定動畫結束時執行的程式
+        CATransaction.begin()
+        CATransaction.setCompletionBlock {
+            //gif動畫_Mickey
+            let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 300, height: 300))
+            mickeyView.addSubview(imageView)
+            let animatedImage = UIImage.animatedImageNamed("mickey-", duration: 1)
+            imageView.image = animatedImage
+            //mask遮罩
+            let maskImageView = UIImageView(image: UIImage(named: "mickeyhead"))
+            maskImageView.frame = imageView.bounds
+            imageView.mask = maskImageView
+            
+            self.fightButton.isHidden = false
+        }
+        //動畫
+        let animation = CABasicAnimation(keyPath: "strokeEnd")
+        animation.fromValue = 0
+        animation.toValue = 1
+        animation.duration = 7
+        mickeyLayer.add(animation, forKey: nil)
+        CATransaction.commit()
+        
+        //gif動畫_喪屍
+        let zombieView = UIImageView(frame: CGRect(x: (view.frame.width - 130)/2, y: 540, width: 130, height: 233))
+        view.addSubview(zombieView)
+        let animatedImage2 = UIImage.animatedImageNamed("zombie-", duration: 1)
+        zombieView.image = animatedImage2
+        
     }
     
 
--- ViewDidLoad/RainViewController.swift
+++ ViewDidLoad/RainViewController.swift
@@ -6,13 +6,101 @@
 //
 
 import UIKit
+import AVFoundation
 
 class RainViewController: UIViewController {
 
+    var bgmlooper: AVPlayerLooper?
+    var shootlooper: AVPlayerLooper?
+    var zombielooper: AVPlayerLooper?
+    var thunderlooper: AVPlayerLooper?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        //背景音樂
+        let bgmUrl = Bundle.main.url(forResource: "bgm02", withExtension: "mp3")!
+        let bgmPlayer = AVQueuePlayer()
+        let bgmItem = AVPlayerItem(url: bgmUrl)
+        bgmlooper = AVPlayerLooper(player: bgmPlayer, templateItem: bgmItem)
+        bgmPlayer.play()
+        
+        //射擊音效
+        let shootUrl = Bundle.main.url(forResource: "shooting", withExtension: "mp3")!
+        let shootPlayer = AVQueuePlayer()
+        let shootItem = AVPlayerItem(url: shootUrl)
+        shootlooper = AVPlayerLooper(player: shootPlayer, templateItem: shootItem)
+        shootPlayer.play()
+        
+        //喪屍音效
+        let zombieUrl = Bundle.main.url(forResource: "zombie", withExtension: "mp3")!
+        let zombiePlayer = AVQueuePlayer()
+        let zombieItem = AVPlayerItem(url: zombieUrl)
+        zombielooper = AVPlayerLooper(player: zombiePlayer, templateItem: zombieItem)
+        zombiePlayer.play()
+        
+        //打雷音效
+        let thunderUrl = Bundle.main.url(forResource: "thunder", withExtension: "mp3")!
+        let thunderPlayer = AVQueuePlayer()
+        let thunderItem = AVPlayerItem(url: thunderUrl)
+        thunderlooper = AVPlayerLooper(player: thunderPlayer, templateItem: thunderItem)
+        thunderPlayer.play()
+        
+        //背景圖
+        let backgroundImageView = UIImageView(image: UIImage(named: "street01"))
+        backgroundImageView.frame = view.bounds
+        backgroundImageView.contentMode = .scaleAspectFill
+        view.addSubview(backgroundImageView)
+        
+        //gif動畫_喪屍
+        let zombieView = UIImageView(frame: CGRect(x: 260, y: 600, width: 80, height: 147))
+        view.addSubview(zombieView)
+        var animatedImage = UIImage.animatedImageNamed("zombie-", duration: 1)
+        zombieView.image = animatedImage
+        
+        //gif動畫_射擊
+        let shootView = UIImageView(frame: CGRect(x: 0, y: 570, width: 225, height: 175))
+        view.addSubview(shootView)
+        animatedImage = UIImage.animatedImageNamed("shoot-", duration: 1)
+        shootView.image = animatedImage
+        
+        //gif動畫_打雷
+        let thunderImageView = UIImageView(frame: CGRect(x: 100, y: -10, width: 200, height: 250))
+        view.addSubview(thunderImageView)
+        animatedImage = UIImage.animatedImageNamed("thunder-", duration: 4)
+        thunderImageView.image = animatedImage
+        
+        //下雨動畫
+        let rainEmitterCell = CAEmitterCell()
+        rainEmitterCell.contents = UIImage(named: "raindrops01")?.cgImage
+        rainEmitterCell.birthRate = 40
+        rainEmitterCell.lifetime = 10
+        rainEmitterCell.velocity = 100
+        rainEmitterCell.xAcceleration = -10
+        rainEmitterCell.yAcceleration = 50
+        rainEmitterCell.scale = 0.6
+        rainEmitterCell.scaleRange = 0.3
+        rainEmitterCell.scaleSpeed = -0.02
+        
+        let rainEmitterCell2 = CAEmitterCell()
+        rainEmitterCell2.contents = UIImage(named: "raindrops02")?.cgImage
+        rainEmitterCell2.birthRate = 20
+        rainEmitterCell2.lifetime = 10
+        rainEmitterCell2.velocity = 100
+        rainEmitterCell2.xAcceleration = -10
+        rainEmitterCell2.yAcceleration = 50
+        rainEmitterCell2.scale = 0.6
+        rainEmitterCell2.scaleRange = 0.3
+        rainEmitterCell2.scaleSpeed = -0.02
+        
+        let rainEmitterLayer = CAEmitterLayer()
+        rainEmitterLayer.emitterCells = [rainEmitterCell, rainEmitterCell2]
+        rainEmitterLayer.emitterPosition = CGPoint(x: view.bounds.width, y: -10)
+        rainEmitterLayer.emitterSize = CGSize(width: view.bounds.width + 200, height: 0)
+        rainEmitterLayer.emitterShape = .line
+        view.layer.addSublayer(rainEmitterLayer)
+        
     }
     
 
