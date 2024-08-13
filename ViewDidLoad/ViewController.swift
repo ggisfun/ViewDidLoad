a24c0aadc5797c487913bf1836b3ed91f0773a28
Adam <bgisgone@gmail.com>
2024-08-13 08:39:32 +0800

    Initial Commit

---
+++ ViewDidLoad/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ViewDidLoad
+//
+//  Created by Adam Chen on 2024/8/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
